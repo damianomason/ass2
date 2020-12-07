53c4da81d6415afc3f799218b95689da16eaf2f9
damianomason <73707211+damianomason@users.noreply.github.com>
2020-12-07 13:02:45 +0100

    corretto typo

--- src/main/java/it/unipd/tos/App.java
+++ src/main/java/it/unipd/tos/App.java
@@ -12,6 +12,6 @@ public class App
     }
     
     public void notCovered() {
-        Sytstem.out.println( "This code is not covered" );
+        System.out.println( "This code is not covered" );
     }
 }
